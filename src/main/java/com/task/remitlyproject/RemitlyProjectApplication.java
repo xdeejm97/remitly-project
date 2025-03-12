a2f1de3394f795ad84a0e18c91a87ce7d1de31a8
xdeejm97 <d.niesmialowski@gmail.com>
2025-03-12 19:34:14 +0100

    Initialize commit

---
+++ src/main/java/com/task/remitlyproject/RemitlyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.task.remitlyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitlyProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RemitlyProjectApplication.class, args);
+  }
+
+}
